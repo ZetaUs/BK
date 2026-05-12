7b960c06c7128bd9495f13767b6ab635e089bcff
Zeta <zeta@example.com>
2026-05-12 13:20:36 +0800

    Change

--- git-up.bat
+++ git-up.bat
@@ -1,6 +1,6 @@
 @echo off
 E:\ZTX\Git\bin\git.exe pull origin main --rebase
 E:\ZTX\Git\bin\git.exe add .
-E:\ZTX\Git\bin\git.exe commit -m "update"
+E:\ZTX\Git\bin\git.exe commit -m "Change"
 E:\ZTX\Git\bin\git.exe push -u origin main
 pause
